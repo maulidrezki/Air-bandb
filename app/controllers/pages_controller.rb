00a4eb3954affb4a01ae73f6dde42c5062d853d7
Maulid Rezki <maulidrezki13@gmail.com>
2021-11-30 17:14:08 +0700

    pages home done with a search button and scss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+
   def home
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name iLike '%#{params[:query]}%'")
+    else
+      @flats = Flat.all
+    end
   end
 
   def dashboard
