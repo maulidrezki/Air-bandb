d23f50bca0d9558330d6bc3de3591244af875d6b
Yeon Kim <awyeon@gmail.com>
2021-11-30 15:07:57 +0800

    dashboard card product scssa changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+def full_name
+  "#{first_name} #{last_name}"
+end
+
 end
