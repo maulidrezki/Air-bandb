823f586746976bc369620ae37b5b8d7067113efb
Yeon Kim <awyeon@gmail.com>
2021-11-30 12:12:30 +0800

    seeding/ gem installed/ cloudinary set

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,10 @@
 class FlatsController < ApplicationController
+
+
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :location, :description, :price_per_night, :capacity, photos: [])
+  end
 end
