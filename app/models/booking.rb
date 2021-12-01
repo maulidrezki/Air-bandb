5918b3312b6b8d519246d7595677ec4c6fd53fda
Maulid Rezki <maulidrezki13@gmail.com>
2021-12-01 20:00:13 +0700

    fixed booking rb and start date end date fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,7 +10,7 @@ class Booking < ApplicationRecord
   # validates :start_date, date: { after_or_equal_to: Date.today }
   validates :end_date, presence: true
 
-  validate :end_date_after_start_date?, :start_date_after_today?, :owner_booking?
+  validate :end_date_after_start_date?, :start_date_after_today? #, :owner_booking?
 
   def end_date_after_start_date?
     if end_date < start_date
