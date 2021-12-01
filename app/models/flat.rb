58a7d41a8d6ad8f080517ea679fabf339583db6e
Pornpan Thongdee <pornpanthongdee02@gmail.com>
2021-12-01 13:13:36 +0800

    added avatar in review card

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -11,4 +11,10 @@ class Flat < ApplicationRecord
   validates :capacity, presence: true, inclusion: { in: 1..100 }
   validates :description, presence: true
   validates :photos, presence: true
+
+  def average_rating
+    return "No reviews" if reviews.length.zero?
+
+    reviews.sum(:rating) / reviews.length.to_f
+  end
 end
