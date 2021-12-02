d52e5809a558c539cf00830dd43f9b13a45845a5
Claudia Manchon <claudiambomb@gmail.com>
2021-12-02 12:40:39 +0800

    user can add avatar in sign in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
 def full_name
   "#{first_name} #{last_name}"
 end
